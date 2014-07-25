785c7e181c40ed3df27452be8b78fa1dd921ead4
luigiprojects <luigi.scarso@gmail.com>
2014-07-26 00:58:28 +0200

    added UserProperties

--- ctx/Perseus_text_1999.03.0053-2.6.0/s-tei-Perseus-text-1999-03-0053-basics.lua
+++ ctx/Perseus_text_1999.03.0053-2.6.0/s-tei-Perseus-text-1999-03-0053-basics.lua
@@ -6,11 +6,37 @@ if not modules then modules = { } end modules ['s-tei-Perseus-text-1999-03-0053-
     license   = "see context related readme files"
 }
 
+document = document or {}
+document.tagged = document.tagged or {}
+
+document.lscarso = document.lscarso or {}
+document.lscarso.userproperties = {}
+
+document.tagged.userproperties = document.lscarso.userproperties
+
+
+document.lscarso.cnt = {}
+
+document.lscarso.cnt.document     = 0
+document.lscarso.cnt.chapter      = 0
+document.lscarso.cnt.section      = 0
+document.lscarso.cnt.sectiontitle = 0
+document.lscarso.cnt.mid          = 0
+document.lscarso.cnt.ignore       = 0
+document.lscarso.cnt.label        = 0
+document.lscarso.cnt.line         = 0
+document.lscarso.cnt.p            = 0
+document.lscarso.cnt.itemgroup    = 0
+document.lscarso.cnt.item         = 0
+document.lscarso.cnt.division     = 0
+
+local report        = logs.reporter("structure:attribute")
+
+
 
 local xml_func = xml.functions or { } 
 local xml_attr = xml.attribute or {}
 -- local tei      = dofile(resolvers.findfile("teitex.lua")) or {}
-
 -- Default binding : flush
 -- for k,v in pairs(tei) do
 --  if type(v) == "function" then
@@ -26,21 +52,28 @@ _div1.act = {}
 
 _div1.act.cast = function(t)
   context.startchapter {title=xml.text(t,"./head"),number="no"} 
+  context.setupelementuserproperties({'section'}, {elementname='div1', type="act",n="cast"})
+  context.setupelementuserproperties({'sectiontitle'}, {elementname='head'})
   lxml.flush(t)
   context.stopchapter()
+
 end
 
 _div1.act.prologue = function(t)
    context.startchapter {title=xml.text(t,"./head"),number="no"} 
+   context.setupelementuserproperties({'section'}, {elementname='div1',type="act",n="prologue"})
+   context.setupelementuserproperties({'sectiontitle'}, {elementname='head'})
    lxml.flush(t)
    context.stopchapter()
-
 end
 
 _div1.act.act = function(t)
   context.startchapter {title=xml.text(t,"./head"),number="no"} 
+  context.setupelementuserproperties({'section'}, {elementname='div1', type="act",})
+  context.setupelementuserproperties({'sectiontitle'}, {elementname='head'})
   lxml.flush(t)
   context.stopchapter()
+
 end
 
 
@@ -62,28 +95,44 @@ local function div2(t)
  local at = t.at
  if (at.type=='scene') then
     context.startsection {title=xml.text(t,"./head"),number="no"} 
+    context.setupelementuserproperties({'section'}, {elementname='div2', type="scene",})
+    context.setupelementuserproperties({'sectiontitle'}, {elementname='head'})
     lxml.flush(t)
-    context.stopchapter()
+    context.stopsection()
  end  
 end
 
 
 local function castList(t)
+   local att = {['elementname']='castList'}
+   context.startelement({"itemgroup"},att)
+   context.setupelementuserproperties({'itemgroup'},att)
    lxml.flush(t)
+   context.stopelement()
 end
 
 local function castItem(t)
+   local at = t.at
+   local att = {['elementname']='castItem'}
    local role    = xml.text(t,"./role")
    local roleDesc= xml.text(t,"./roleDesc")
+   att.type=at.type 
+   context.startelement({"item"})
+   context.setupelementuserproperties({'item'},att)
    if string.len(roleDesc)>0 then 
       context([[\hbox{\hbox to 11em{\bf ]] .. role .. [[:\hss}\hskip1em{\it ]] .. roleDesc .. "}}")
    else 
       context("{\\bf " .. role .. "}")
    end
    context.blank()
+   context.stopelement()
 end
 
 local function castGroup(t)
+   local at = t.at
+   local att = {['elementname']='castGroup'}
+   context.startelement({"division"},att)
+   context.setupelementuserproperties({'division'},att)
    local rend  = (xml.text(t,"./head[@rend=='braced']") ~= "" ) 
    if rend then
       local desc = xml.text(t,"./head")
@@ -114,28 +163,68 @@ local function castGroup(t)
    else 
       lxml.flush(t)
    end 
+   context.stopelement()
 end
 
 local function l(t) 
+   local att = {elementname='l'}
+   context.startelement({"line"},att)
+   context.setupelementuserproperties({'line'}, att )
    lxml.flush(t)
+   context.stopelement()
+end
+
+
+local function lb(t) 
+   local at = t.at
+   local att = {elementname='lb'}
+   if (at.ed ~=nil) then
+      att['ed']= at.ed 
+   end
+   if (at.n ~=nil) then
+      att['n']= at.n 
+   end
+   context.startelement({"ignore"},att)
+   context.setupelementuserproperties({'ignore'}, att )
+   context.SYMBOLFORNEWLINE();context.par()
+   context.stopelement()
 end
 
 
 
 local function p(t) 
+   local att = {elementname='p'}
+   context.startelement({"p"},att)
+   context.setupelementuserproperties({'p'}, att )
    lxml.flush(t)
+   context.stopelement()
+
 end
 
 local function sp(t) 
+   local at = t.at
+   local att = {elementname='sp'}
+   att.who = at.who or nil
+   context.startelement({"p"},att)
+   context.setupelementuserproperties({'p'}, att )
    lxml.flush(t)
+   context.stopelement()
 end
 
 local function speaker(t) 
+   local att = {elementname='speaker'}
    context.blank()
+   context.startelement({"label"},att)
+   context.setupelementuserproperties({'label'}, att )
    context([[{\it ]] .. xml.text(t,"./") .. [[}\hskip0.5em]])
+   context.stopelement()
+
 end
 
 local function stage(t) 
+   local att = {elementname='speaker'}
+   context.startelement({"mid"},att)
+   context.setupelementuserproperties({'mid'}, {elementname='stage',})
    context.blank()
    context([[{\sc ]])
    context.startalignment({"middle"})
@@ -143,6 +232,7 @@ local function stage(t)
    context.stopalignment()
    context([[}]])
    context.blank()
+   context.stopelement()
 end
 
 
@@ -156,13 +246,13 @@ xml_func["tei:body"] = lxml.flush
 
 xml_func["tei:div1"] = div1
 xml_func["tei:div2"] = div2
-xml_func["tei:head"] = nil  -- depends on the context 
+xml_func["tei:head"] = function(t) end  -- depends on the context 
 
 xml_func["tei:castList"] = castList
 xml_func["tei:castItem"] = castItem
 xml_func["tei:castGroup"] = castGroup
 
-xml_func["tei:lb"] = function(t) context.par() end
+xml_func["tei:lb"] = lb -- function(t) context.par() end
 
 xml_func["tei:l"] = l
 xml_func["tei:p"] = p
